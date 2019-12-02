3ebcfddb44038475b90a003e956a4b49837655a8
abdielantonioxd <abdielantonio.af@gmail.com>
2019-12-01 21:46:25 -0500

    connector v1.1v0

--- source/plugdo/collector/mongo-collector.js
+++ source/plugdo/collector/mongo-collector.js
@@ -2,7 +2,7 @@ var models_user = global.models.user;
 var connection = global.mongodb.server;
 plugdo.collector("mongoInsert", {
   type: "db",
-  action: "mongodb",
+  action: "mongodb",  
   server: connection,
   collection: "plugdo",
   queryType: "insert",
